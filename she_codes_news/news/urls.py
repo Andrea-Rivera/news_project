5406594d2d822c8889905cdba720708c524430a0
Andrea <andita54@hotmail.com>
2023-07-20 15:22:25 +1000

    updating README, Edit and Delete features

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -6,5 +6,8 @@ app_name = 'news'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
-    path('add-story/', views.AddStoryView.as_view(), name='newStory')
+    path('add-story/', views.AddStoryView.as_view(), name='newStory'),
+    path('<int:pk>/update/', views.UpdateStoryView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.DeleteStoryView.as_view(), name='delete'),
+ 
 ]
--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import NewsStory
 from .forms import StoryForm
 
@@ -24,7 +25,7 @@ class StoryView(generic.DetailView):
     template_name = 'news/story.html'
     context_object_name = 'story'
 
-class AddStoryView(generic.CreateView):
+class AddStoryView(LoginRequiredMixin,generic.CreateView):
     form_class = StoryForm 
     context_object_name = 'storyform' 
     template_name = 'news/createStory.html' 
@@ -33,3 +34,35 @@ class AddStoryView(generic.CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class UpdateStoryView(LoginRequiredMixin,UserPassesTestMixin,generic.UpdateView):
+    model = NewsStory
+    form_class = StoryForm 
+    context_object_name = 'storyform' 
+    template_name = 'news/createStory.html' 
+    success_url = reverse_lazy('news:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    #Only the author of the post can update the post
+    def test_func(self):
+        story = self.get_object()
+        if self.request.user == story.author:
+            return True
+        return False
+    
+class DeleteStoryView(LoginRequiredMixin,UserPassesTestMixin,generic.DeleteView):
+    model = NewsStory
+    context_object_name = 'deleteStory' 
+    template_name = 'news/deleteStory.html' 
+    success_url = reverse_lazy('news:index')
+
+
+        #Only the author of the post can delete the post
+    def test_func(self):
+        story = self.get_object()
+        if self.request.user == story.author:
+            return True
+        return False
