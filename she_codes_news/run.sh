96a66ef5351ff22630865f205ac8835d8f1ab6cb
Andrea <andita54@hotmail.com>
2023-08-17 20:44:07 +1000

    changing run.sh and fly.toml

--- she_codes_news/run.sh
+++ she_codes_news/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 python manage.py migrate
 python manage.py createsuperuser --no-input
-gunicorn --bind :8000 --workers 1 crowdfunding.wsgi
+gunicorn --bind :8000 --workers 1 she_codes_news.wsgi
 
